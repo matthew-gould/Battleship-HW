5de4e0d475af4950d463e1ada8dca8d78be811d3
Katie Walsh <katie.katiewalsh@gmail.com>
2015-02-05 16:07:42 -0500

    Ship finally know if it is horizontal

--- battleship_test.rb
+++ battleship_test.rb
@@ -1,5 +1,6 @@
 require 'minitest/autorun'
 require './ship'
+require 'pry'
 
 
 class BattleshipTests < MiniTest::Test
@@ -12,9 +13,14 @@ class BattleshipTests < MiniTest::Test
   def test_ship_is_horizontal_or_vertical
     ship = Ship.new "v", 3
     assert ship.vertical?, true
+
+    ship_two = Ship.new "h", 3
+    refute ship_two.vertical?
   end
 
-
+  # def test_if_the_ship_knows_it_has_been_hit
+  # 	ship = Ship.new "h"
+  # end
 
 
 
--- ship.rb
+++ ship.rb
@@ -1,17 +1,19 @@
+require 'pry'
 class Ship
 
-  attr_reader :vertical, :length
+  attr_reader :length
 
-  def initialize vertical, length=0
+  def initialize position, length=0
     @length = length.to_i
-    @vertical = vertical.to_s
+    @position = position.to_s
   end
 
   def vertical?
-    if @vertical.include?("v")
-      return true
-    else @vertical.include?("h")
-      return false
+
+    if @position == "v"
+      true
+    elsif @position == "h"
+      false
     end
   end
 
