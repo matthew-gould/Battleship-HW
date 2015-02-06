750239ccc7f857e23a3c0b9c8bc3b3991774f8a2
Katie Walsh <katie.katiewalsh@gmail.com>
2015-02-06 11:36:16 -0500

    Thinking about how to make a coordinate class

---
+++ coordinate_class.rb
@@ -0,0 +1,27 @@
+require 'pry'
+# Thoughts on possible Coordinate class
+
+# class Coordinate
+# 	attr_reader :row, :column, :coor_array
+
+# 	def initialize (row, column)
+# 	@coor_array= []
+# 	@row = row
+# 	@column = column
+# 	coor_array.push(@row, column)
+# 	@occupied = false
+# 	end
+
+# 	def shot? 
+# 		if @occupied
+# 			# something that says it has been hit
+# 		else
+# 			# something that says it was a miss
+# 		end
+
+# 	end
+
+# 	def assign
+# 		@occupied = true
+# 	end
+# end
