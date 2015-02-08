ae4192125a052ffd630eb7c708ab49aa1f75c285
David Livingston <dave.livingston@comcast.net>
2015-02-08 16:54:31 -0500

    Minor edits

--- battleship_play.rb
+++ battleship_play.rb
@@ -12,14 +12,16 @@ puts "\n\n******************************"
 puts "****Welcome to BATTLESHIP!****"
 puts "******************************\n\n"
 
-2.times do |x|
-puts "Player#{x+1}, what is your name!?"
-name = gets.chomp.downcase.capitalize
-
-Player.new(name)
-Game.new
-
+players = []
+(1..2).each do |x|
+	puts "Player #{x}, what is your name!?"
+	name = gets.chomp.downcase.capitalize
+	players << Player.new(name)
 end
+
+#Game.new
+puts "Player 1 is #{players[0].name}"
+puts "Player 2 is #{players[1].name}"
 
 puts "Let's set up your ships!"
 
